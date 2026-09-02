4e92b348ac910d6f135fb511078e9136cdc30da5
Nataniel Appasamy <nappasam@k2r3p6.42mulhouse.fr>
2026-09-02 16:59:24 +0200

    added connection class

--- zone.py
+++ zone.py
@@ -14,6 +14,8 @@ class Zone:
         if zone_type not in self.VALID_ZONE:
             raise ValueError('Unknown zone type')
         self.zone_type = zone_type
+        if max_cap <= 0:
+            raise ValueError('max cap must be above 0')
         self.max_cap = max_cap
         self.color = color
         self.occupants: list[int] = []
@@ -50,3 +52,43 @@ class Zone:
             return True
         else:
             return False
+
+
+class Connection:
+    def __init__(self, zone_a: Zone, zone_b: Zone, max_link_capacity: int = 1):
+        self.zone_a = zone_a
+        self.zone_b = zone_b
+        if max_link_capacity <= 0:
+            raise ValueError('max cap must be above 0')
+        self.max_link_capacity = max_link_capacity
+        self.drones: list[int] = []
+
+    def crossing(self) -> int:
+        return len(self.drones)
+
+    def can_cross(self) -> bool:
+        if len(self.drones) >= self.max_link_capacity:
+            return False
+        else:
+            return True
+
+    def add_drone(self, drone: int) -> None:
+        if self.can_cross():
+            self.drones.append(drone)
+        else:
+            raise ValueError('no more room')
+
+    def remove_drone(self, drone: int) -> None:
+        self.drones.remove(drone)
+
+    def normalized_key(self) -> tuple[str, str]:
+        first, second = sorted((self.zone_a.name, self.zone_b.name))
+        return (first, second)
+
+    def end_point(self, zone: Zone) -> Zone:
+        if zone.name == self.zone_a.name:
+            return self.zone_b
+        elif zone.name == self.zone_b.name:
+            return self.zone_a
+        else:
+            raise ValueError('zone is not an endpoint of this connection')
